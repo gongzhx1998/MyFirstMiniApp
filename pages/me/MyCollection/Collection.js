0a44506f3e1e7c07e22c0ad3ef54a16fc5e54631
宫志鑫 <2294835989@qq.com>
2020-04-03 22:24:53 +0800

    做好了查看我的收藏页面

--- pages/me/MyCollection/Collection.js
+++ pages/me/MyCollection/Collection.js
@@ -5,19 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-    initData:''
+    initData: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let that=this;
-    let eventChannel=this.getOpenerEventChannel();
-    eventChannel.on('sendData',function(e){
+    let that = this;
+    let eventChannel = this.getOpenerEventChannel();
+    eventChannel.on('sendData', function (e) {
+      function fn(e) {
+        for (let item of e.data) {
+          let temp = '';
+          temp = item.Content.substr(0, 30);
+          item.Content = temp+'...';          
+        }
+      }
+      fn(e)
       that.setData({
-        initData:e
-      })        
+        initData: e
+      })
     });
   },
 
