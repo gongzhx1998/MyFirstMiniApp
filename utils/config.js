0e8554c5a16794f595ffed329cefa84c2f947bda
gongzhx1998 <2294835989@qq.com>
2020-07-03 17:20:15 +0800

    Update config.js
    jghjf

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
-var fileHost='https://oss-legchina.oss-cn-beijing.aliyuncs.com/';
+var fileHost='https://.aliyuncs.com/';
 
 var config={
   uploadImageUrl:`${fileHost}`,
-  AccessKeySecret:'xx30999uXOgZrphFsHD2D5ZpVyqlWZCw',
-  OSSAccessKeyId:'L29yokLG1XcSWuuYEhd6dazNqb',
+  AccessKeySecret:'xx30999uXyqlWZCw',
+  OSSAccessKeyId:'YEhd6dazNqb',
   timeout:87600
 };
 module.exports=config;
